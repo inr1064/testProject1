4148802be581a13b51d3a760fe540106e7352906
Han1992 <inr1064@naver.com>
2018-03-27 14:06:34 +0900

    x
    z

--- src/main/java/com/testPro/www/PDFController.java
+++ src/main/java/com/testPro/www/PDFController.java
@@ -1,36 +1,15 @@
 package com.testPro.www;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-
-import javax.imageio.ImageIO;
-
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.rendering.PDFRenderer;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PDFController {
-	
-	@RequestMapping(value="/pdf",method=RequestMethod.GET)
-	public String PDFpage(Model model) throws Exception
+
+	@RequestMapping(value="pdf",method=RequestMethod.GET)
+	public String pdf()
 	{
-		
-		File file = new File("C:/java/java/Spring/testProject1/src/main/webapp/resources/sample.pdf");
-		PDDocument doc = PDDocument.load(file);
-		
-		PDFRenderer renderer = new PDFRenderer(doc);
-		
-		BufferedImage image = renderer.renderImage(0);
-		
-		ImageIO.write(image, "JPEG", new File("C:/java/java/Spring/testProject1/src/main/webapp/resources/images/myimage.jpg"));
-		
-		doc.close();
-		
 		return "PDFpage";
 	}
-
 }
